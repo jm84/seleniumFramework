93e8c46731e15797163ff99fc61f9a911718c3a2
jm84 <jignamolina@gmail.com>
2022-06-28 00:21:52 +0200

    cucumber structure

---
+++ src/test/java/stepDefinitions/Contact_Us_Steps.java
@@ -0,0 +1,45 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Contact_Us_Steps {
+
+    @Given("I access the webdriver university contact us page")
+    public void i_access_the_webdriver_university_contact_us_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I enter a unique first name")
+    public void i_enter_a_unique_first_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @And("I enter a unique last name")
+    public void i_enter_a_unique_last_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @And("I enter a unique email address")
+    public void i_enter_a_unique_email_address() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @And("I enter a unique comment")
+    public void i_enter_a_unique_comment() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @And("I click on the submit button")
+    public void i_click_on_the_submit_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("I should be presented with a successfull contact us submission message")
+    public void i_should_be_presented_with_a_successfull_contact_us_submission_message() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
